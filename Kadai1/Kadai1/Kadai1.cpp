89eeb6c54228b4532ea352d5c7d5adc2c9e90050
AURAjp <basket.3522@gmail.com>
2021-10-08 00:13:26 +0900

    feat: OpenCV依存先を2.4.13に変更

--- Kadai1/Kadai1/Kadai1.cpp
+++ Kadai1/Kadai1/Kadai1.cpp
@@ -6,10 +6,22 @@ using namespace std;
 
 #ifdef _DEBUG
 // debug用のライブラリをリンク
-#pragma comment(lib, "opencv_world453d.lib")
+#pragma comment(lib, "opencv_legacy2413d.lib") 
+#pragma comment(lib, "opencv_calib3d2413d.lib") 
+#pragma comment(lib, "opencv_core2413d.lib") 
+#pragma comment(lib, "opencv_objdetect2413d.lib") 
+#pragma comment(lib, "opencv_features2d2413d.lib") 
+#pragma comment(lib, "opencv_imgproc2413d.lib") 
+#pragma comment(lib, "opencv_highgui2413d.lib") 
 #else
 // Release用のライブラリをリンク
-#pragma comment(lib, "opencv_world453.lib")
+#pragma comment(lib, "opencv_legacy2413.lib") 
+#pragma comment(lib, "opencv_core2413.lib") 
+#pragma comment(lib, "opencv_imgproc2413.lib") 
+#pragma comment(lib, "opencv_highgui2413.lib") 
+#pragma comment(lib, "opencv_objdetect2413.lib") 
+#pragma comment(lib, "opencv_features2d2413.lib") 
+#pragma comment(lib, "opencv_calib3d2413.lib") 
 #endif
 
 int main() {
