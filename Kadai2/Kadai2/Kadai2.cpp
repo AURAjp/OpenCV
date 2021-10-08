6a1e466fe605be959e1f72169ebee2f2d59520d8
AURAjp <basket.3522@gmail.com>
2021-10-08 10:59:02 +0900

    feat: 課題2用の暫定ロジックを追加

--- Kadai2/Kadai2/Kadai2.cpp
+++ Kadai2/Kadai2/Kadai2.cpp
@@ -27,8 +27,11 @@ using namespace std;
 int main() {
 
     // 画像ファイルを読み込む
-    Mat srcImg = imread("./img/in.jpg");
-    if (srcImg.empty()) {
+    Mat srcImg    = imread("./img/in.jpg");
+    // フィルタ用画像をグレースケールで読み込む
+    Mat filterImg = imread("./img/psf.png", 0);
+    // 読み込んだ画像のNULLチェック
+    if (srcImg.empty() || filterImg.empty()) {
         return -1;
     }
 
@@ -39,15 +42,27 @@ int main() {
     // アスペクト比を保持した高さ
     int height = aspectRatio * width;
 
-    // 出力用画像領域の確保
-    Mat dstImg(height, width, srcImg.type());
+    // リサイズ用画像領域の確保
+    Mat resizeImg(height, width, srcImg.type());
     // リサイズ
-    resize(srcImg, dstImg, dstImg.size());
+    resize(srcImg, resizeImg, resizeImg.size());
+
+    // 画像をdouble型に変換
+    filterImg.convertTo(filterImg, CV_64F);
+    // 総和を求める
+    double sumImg = (double) sum(filterImg)[0];
+    // 総和で割る（総和が1 になる＝画像明るさ保存）
+    filterImg = filterImg * (1. / sumImg);
+
+    // 出力用画像領域の確保
+    Mat outImg(height, width, srcImg.type());
+    // リサイズした画像にフィルターをかける
+    filter2D(resizeImg, outImg, 8, filterImg);
 
     // 出力画像を表示
-    imshow("img", dstImg);
+    imshow("img", outImg);
     // 出力画像を保存
-    imwrite("./img/out.jpg", dstImg);
+    imwrite("./img/out.jpg", outImg);
 
     // キーボードが押されるまで処理を待つ
     waitKey(0);
