3a8b0e3c5f3413f0aa8982740fcc4e5735aef707
AURAjp <basket.3522@gmail.com>
2021-10-17 00:10:30 +0900

    feat: 課題3のロジックを追加。ただし論理積の演算でExceptionが発生する。

--- Kadai3/Kadai3/Kadai3.cpp
+++ Kadai3/Kadai3/Kadai3.cpp
@@ -33,37 +33,44 @@ int main() {
         return -1;
     }
 
-    // 画像をリサイズ
-    Mat resizeImg = resizeImage(srcImg);
-
-    // Canny法によるエッジ検出
-    Mat cannyImg;
-
-    Canny(resizeImg, cannyImg, 100, 400, 3, true);
-
-    // 出力画像を表示
-    imshow("cannyImg", cannyImg);
-    // 出力画像を保存
-    imwrite("./img/cannyImg.jpg", cannyImg);
-
-    // キーボードが押されるまで処理を待つ
-    waitKey(0);
-
-    return 0;
-}
-
-Mat resizeImage(Mat image) {
     // 入力画像のアスペクト比
-    double aspectRatio = (double)image.rows / image.cols;
+    double aspectRatio = (double)srcImg.rows / srcImg.cols;
     // 出力画像の横幅
     int width = 800;
     // アスペクト比を保持した高さ
     int height = aspectRatio * width;
 
     // リサイズ用画像領域の確保
-    Mat resizeImg(height, width, image.type());
+    Mat resizeImg(height, width, srcImg.type());
     // リサイズ
-    resize(image, resizeImg, resizeImg.size());
+    resize(srcImg, resizeImg, resizeImg.size());
 
-    return resizeImg;
+    // Canny法によるエッジ検出用画像領域の確保
+    Mat cannyImg;
+    // しきい値100&300、ApertureSize=3、画像勾配の精度向上=true
+    Canny(resizeImg, cannyImg, 100., 300., 3, true);
+    // エッジ検出画像の色を反転
+    bitwise_not(cannyImg, cannyImg);
+
+    //Bilateralフィルタ処理用画像領域確保
+    Mat bilateralImg;
+    bilateralFilter(resizeImg, bilateralImg, 15, 30., 30., BORDER_DEFAULT);
+
+    // 論理積を計算
+    Mat outImg;
+    bitwise_and(bilateralImg, cannyImg, outImg);
+
+    // 出力画像を表示
+    imshow("Canny", cannyImg);
+    imshow("Bilateral", bilateralImg);
+    imshow("outImg", outImg);
+
+    // 出力画像を保存
+    imwrite("canny.png", cannyImg);
+    imwrite("bilateral.png", bilateralImg);
+    imwrite("out.png", outImg);
+
+    waitKey(0);
+
+    return 0;
 }
