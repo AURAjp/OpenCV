a1b56e7e17644f2bed37682d57f0e6f9bddb7030
AURAjp <basket.3522@gmail.com>
2021-10-17 14:50:26 +0900

    feat: 変数の命名規則をスネークケースに変更

--- Kadai3/Kadai3/Kadai3.cpp
+++ Kadai3/Kadai3/Kadai3.cpp
@@ -27,52 +27,52 @@ using namespace std;
 int main() {
 
     // 画像ファイルを読み込む
-    Mat srcImg = imread("./img/in.jpg", IMREAD_COLOR);
+    Mat src_img = imread("./img/in.jpg", IMREAD_COLOR);
     // 読み込んだ画像のNULLチェック
-    if (srcImg.empty()) {
+    if (src_img.empty()) {
         return -1;
     }
 
     // 入力画像のアスペクト比
-    double aspectRatio = (double)srcImg.rows / srcImg.cols;
+    double aspect_ratio = (double)src_img.rows / src_img.cols;
     // 出力画像の横幅
     constexpr int width = 800;
     // アスペクト比を保持した高さ
-    int height = aspectRatio * width;
+    int height = aspect_ratio * width;
 
     // リサイズ用画像領域の確保
-    Mat resizeImg(height, width, srcImg.type());
+    Mat resize_img(height, width, src_img.type());
     // リサイズ
-    resize(srcImg, resizeImg, resizeImg.size());
+    resize(src_img, resize_img, resize_img.size());
 
     // Canny法によるエッジ検出用画像領域の確保
-    Mat cannyImg;
+    Mat canny_img;
     // しきい値100&300、ApertureSize=3、画像勾配の精度向上=true
-    Canny(resizeImg, cannyImg, 100., 300., 3, true);
+    Canny(resize_img, canny_img, 100., 300., 3, true);
     // エッジ検出画像の色を反転
-    bitwise_not(cannyImg, cannyImg);
+    bitwise_not(canny_img, canny_img);
     // 論理積を計算するためにCV_8UのC1からCV_8UのC3に変換
-    cvtColor(cannyImg, cannyImg, CV_GRAY2BGR);
+    cvtColor(canny_img, canny_img, CV_GRAY2BGR);
 
     // Bilateralフィルタ処理用画像領域確保
-    Mat bilateralImg;
+    Mat bilateral_img;
     // Bilateralフィルタ適用
-    bilateralFilter(resizeImg, bilateralImg, 15, 30., 30., BORDER_DEFAULT);
+    bilateralFilter(resize_img, bilateral_img, 15, 30., 30., BORDER_DEFAULT);
 
     // 出力画像領域の確保
-    Mat outImg;
+    Mat out_img;
     // 論理積を計算
-    bitwise_and(bilateralImg, cannyImg, outImg);
+    bitwise_and(bilateral_img, canny_img, out_img);
 
     // 出力画像を表示
-    imshow("Canny", cannyImg);
-    imshow("Bilateral", bilateralImg);
-    imshow("Out", outImg);
+    imshow("Canny", canny_img);
+    imshow("Bilateral", bilateral_img);
+    imshow("Out", out_img);
 
     // 出力画像を保存
-    imwrite("./img/canny.png", cannyImg);
-    imwrite("./img/bilateral.png", bilateralImg);
-    imwrite("./img/out.png", outImg);
+    imwrite("./img/canny.png", canny_img);
+    imwrite("./img/bilateral.png", bilateral_img);
+    imwrite("./img/out.png", out_img);
 
     // キーボードが押されるまで処理を待つ
     waitKey(0);
