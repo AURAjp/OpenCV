fae5e4d282f6ab717426bb05624ba6910d78c04f
AURAjp <basket.3522@gmail.com>
2021-10-17 15:37:02 +0900

    feat: 暫定ロジックの追加

--- Kadai4/Kadai4.cpp
+++ Kadai4/Kadai4.cpp
@@ -1,20 +1,28 @@
-﻿// Kadai4.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace cv;
+
+#ifdef _DEBUG
+// debug用のライブラリをリンク
+#pragma comment(lib, "opencv_legacy2413d.lib")
+#pragma comment(lib, "opencv_calib3d2413d.lib")
+#pragma comment(lib, "opencv_core2413d.lib")
+#pragma comment(lib, "opencv_objdetect2413d.lib")
+#pragma comment(lib, "opencv_features2d2413d.lib")
+#pragma comment(lib, "opencv_imgproc2413d.lib")
+#pragma comment(lib, "opencv_highgui2413d.lib")
+#else
+// Release用のライブラリをリンク
+#pragma comment(lib, "opencv_legacy2413.lib")
+#pragma comment(lib, "opencv_core2413.lib")
+#pragma comment(lib, "opencv_imgproc2413.lib")
+#pragma comment(lib, "opencv_highgui2413.lib")
+#pragma comment(lib, "opencv_objdetect2413.lib")
+#pragma comment(lib, "opencv_features2d2413.lib")
+#pragma comment(lib, "opencv_calib3d2413.lib")
+#endif
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
