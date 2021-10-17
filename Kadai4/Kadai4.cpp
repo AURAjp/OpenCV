bdf6b0e995b1a8c7c3519f4172c57f25840762c4
AURAjp <basket.3522@gmail.com>
2021-10-17 15:55:49 +0900

    feat: リサイズ用ロジックの追加

--- Kadai4/Kadai4.cpp
+++ Kadai4/Kadai4.cpp
@@ -24,5 +24,27 @@ using namespace cv;
 
 int main()
 {
+	// 画像ファイルを読み込む
+	Mat src_img = imread("./img/in.jpg", IMREAD_COLOR);
+	// 読み込んだ画像のNULLチェック
+	if (src_img.empty()) {
+		return -1;
+	}
+
+	// 入力画像のアスペクト比
+	double aspect_ratio = (double)src_img.rows / src_img.cols;
+	// 出力画像の横幅
+	constexpr int WIDTH = 800;
+	// アスペクト比を保持した高さ
+	int height = aspect_ratio * WIDTH;
+
+	// リサイズ用画像領域の確保
+	Mat resize_img(height, WIDTH, src_img.type());
+	// リサイズ
+	resize(src_img, resize_img, resize_img.size());
+
+	// キーボードが押されるまで処理を待つ
+	waitKey(0);
+
 	return 0;
 }
