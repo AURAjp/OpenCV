22ffee428789686010d6c0e445d69b9d265dd5c9
AURAjp <basket.3522@gmail.com>
2021-10-23 23:12:30 +0900

    feat: 境界値を考慮したロジックに修正

--- Kadai5/Kadai5.cpp
+++ Kadai5/Kadai5.cpp
@@ -38,45 +38,55 @@ void my_medianBlur(const Mat& in, Mat& out, int filter_size)
         in.copyTo(out);
         return;
     }
-    // フィルタサイズが奇数かどうかチェック
-    CV_Assert(filter_size % 2 == 1);
+    // フィルタサイズが奇数か、入力画像がCV_8UC1かどうかチェック
+    CV_Assert(filter_size % 2 == 1 && in.type() == 0);
 
-    // 処理範囲を特定する
-    const int width = in.cols;
-    const int height = in.rows;
     // フィルタが影響を与えるのは処理対象ピクセルデータから前後半分のエリア
     const int affect_area = filter_size / 2;
-    // 周辺領域のピクセルを格納する配列
+
+    // 境界処理用に画像の上下左右にフィルタが影響を与えるピクセル分を反転させて追加する
+    Mat border_extend_img;
+    copyMakeBorder(in, border_extend_img, affect_area, affect_area, affect_area, affect_area, BORDER_REFLECT_101);
+
+    // 処理範囲を特定する
+    const int width = border_extend_img.cols;
+    const int height = border_extend_img.rows;
+
+    // 周辺領域のピクセルを格納する動的配列
     vector<int> pixel_around(pow(filter_size, 2), 0);
     // 出力用画像領域の確保
-    out = Mat(height, width, CV_8UC1);
+    Mat dst_img = Mat(height, width, CV_8UC1);
 
     /**
-    * 画像データの(1, 1) -> (width - 1, height - 1) までループ.<br>
+    * 処理対象ピクセルデータ(x, y)について、
+    * (affect_area, affect_area) -> (width - affect_area, height - affect_area) までループ.<br>
     * (0, 0)や(width, height)などの境界値は最終的にトリミングするため無視する.
     */
     for (int y = affect_area; y < height - affect_area; y++)
     {
         for (int x = affect_area; x < width - affect_area; x++)
         {
-            int count = 0;
+            // 配列の添字
+            int index = 0;
             // (x - affect_area, y - affect_area) -> (x + affect_area, y + affect_area)の範囲にフィルターをかける
             for (int py = y - affect_area; py <= y + affect_area; py++)
             {
                 for (int px = x - affect_area; px <= x + affect_area; px++)
                 {
                     // 処理対象ピクセルデータの周辺領域のピクセルデータを保存
-                    pixel_around[count] = in.at<unsigned char>(py, px);
-                    count++;
+                    pixel_around[index] = border_extend_img.at<unsigned char>(py, px);
+                    index++;
                 }
             }
             // 保存したピクセルデータをソートして中央値を取得
             sort(begin(pixel_around), end(pixel_around));
             int median = pixel_around[pixel_around.size() / 2 + 1];
             // 取得した中央値を処理対象ピクセルに上書きする
-            out.at<unsigned char>(y, x) = median;
+            dst_img.at<unsigned char>(y, x) = median;
         }
     }
+    // 境界処理用に拡張した部分をトリミング
+    out = Mat(dst_img, Rect(affect_area, affect_area, width - affect_area, height - affect_area));
 }
 
 int main()
@@ -101,41 +111,22 @@ int main()
     // リサイズ
     resize(src_img, resize_img, resize_img.size());
 
-    // 境界処理用に画像の上下左右に1ピクセルを反転させて追加する
-    constexpr int EXPIXEL = 1;
-    Mat border_extend_img;
-    copyMakeBorder(resize_img, border_extend_img, EXPIXEL, EXPIXEL, EXPIXEL, EXPIXEL, BORDER_REFLECT_101);
-
     // Median Blur
-    Mat extend_out05_img;
-    medianBlur(border_extend_img, extend_out05_img,  5);
-    Mat extend_out11_img;
-    medianBlur(border_extend_img, extend_out11_img, 11);
-    Mat extend_out21_img;
-    medianBlur(border_extend_img, extend_out21_img, 21);
-
-    // 境界処理用に拡張した部分をトリミング
-    const Mat out05_img(extend_out05_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
-    const Mat out11_img(extend_out11_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
-    const Mat out21_img(extend_out21_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
-
-    Mat out03_img;
-    medianBlur(resize_img, out03_img, 3);
-    Mat extend_out03_img;
-    my_medianBlur(border_extend_img, extend_out03_img, 3);
-    const Mat my_out03_img(extend_out03_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
-
-    imshow("out03"  , out03_img);
-    imshow("myout03", my_out03_img);
+    Mat out05_img;
+    my_medianBlur(resize_img, out05_img,  5);
+    Mat out11_img;
+    my_medianBlur(resize_img, out11_img, 11);
+    Mat out21_img;
+    my_medianBlur(resize_img, out21_img, 21);
 
     // 結果表示
-    //imshow("out05", out05_img);
-    //imshow("out11", out11_img);
-    //imshow("out21", out21_img);
+    imshow("out05", out05_img);
+    imshow("out11", out11_img);
+    imshow("out05", out21_img);
 
-    //imwrite("./img/out5.png" , out05_img);
-    //imwrite("./img/out11.png", out11_img);
-    //imwrite("./img/out21.png", out21_img);
+    imwrite("./img/out5.png" , out05_img);
+    imwrite("./img/out11.png", out11_img);
+    imwrite("./img/out21.png", out21_img);
 
     // キーボードが押されるまでwait
     waitKey(0);
