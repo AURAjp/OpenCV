f9783b2506eb72328cae8fc57c960ce3fb549872
AURAjp <basket.3522@gmail.com>
2021-10-23 21:23:59 +0900

    フィルタ用ロジックを追加

--- Kadai5/Kadai5.cpp
+++ Kadai5/Kadai5.cpp
@@ -39,6 +39,36 @@ void my_medianBlur(const Mat& in, Mat& out, int filter_size)
     }
     // フィルタサイズが奇数かどうかチェック
     CV_Assert(filter_size % 2 == 1);
+
+    const int width = in.cols;
+    const int height = in.rows;
+
+    // 周辺領域のピクセルを格納する配列
+    // TODO: 暫定で3*3フィルタ
+    int pixel_around[9];
+
+    /**
+    * 画像データの(1, 1) -> (width - 1, height - 1) までループ.<br>
+    * (0, 0)や(width, height)などの境界値は最終的にトリミングするため無視する.
+    */
+    for (int y = 1; y < height - 1; y++)
+    {
+        for (int x = 1; x < width - 1; x++)
+        {
+            int count = 0;
+            // (x - 1, y - 1) -> (x + 1, y + 1)の範囲にフィルターをかける
+            for (int py = y - 1; py <= y + 1; py++)
+            {
+                for (int px = x - 1; px <= x + 1; px++)
+                {
+                    // 処理対象ピクセルデータの周辺領域のピクセルデータを保存
+                    pixel_around[count] = in.at<unsigned char>(py, px);
+                    count++;
+                }
+            }
+            // 保存したピクセルデータをソートする
+        }
+    }
 }
 
 int main()
@@ -63,8 +93,8 @@ int main()
     // リサイズ
     resize(src_img, resize_img, resize_img.size());
 
-    // 境界処理用画像領域の確保
-    constexpr int EXPIXEL = 100;
+    // 境界処理用に画像の上下左右に1ピクセルを反転させて追加する
+    constexpr int EXPIXEL = 1;
     Mat border_extend_img;
     copyMakeBorder(resize_img, border_extend_img, EXPIXEL, EXPIXEL, EXPIXEL, EXPIXEL, BORDER_REFLECT_101);
 
@@ -81,14 +111,23 @@ int main()
     const Mat out11_img(extend_out11_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
     const Mat out21_img(extend_out21_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
 
-    // 結果表示
-    imshow("out05", out05_img);
-    imshow("out11", out11_img);
-    imshow("out21", out21_img);
+    Mat out03_img;
+    medianBlur(resize_img, out03_img, 3);
+    Mat extend_out03_img;
+    my_medianBlur(border_extend_img, extend_out03_img, 3);
+    ///const Mat my_out03_img(extend_out03_img, Rect(EXPIXEL, EXPIXEL, WIDTH, height));
 
-    imwrite("./img/out5.png" , out05_img);
-    imwrite("./img/out11.png", out11_img);
-    imwrite("./img/out21.png", out21_img);
+    imshow("out03"  , out03_img);
+    //imshow("myout03", my_out03_img);
+
+    // 結果表示
+    //imshow("out05", out05_img);
+    //imshow("out11", out11_img);
+    //imshow("out21", out21_img);
+
+    //imwrite("./img/out5.png" , out05_img);
+    //imwrite("./img/out11.png", out11_img);
+    //imwrite("./img/out21.png", out21_img);
 
     // キーボードが押されるまでwait
     waitKey(0);
