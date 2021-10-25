79833ede7677939f12b26d8331751fe468a470f0
AURAjp <basket.3522@gmail.com>
2021-10-25 19:17:52 +0900

    feat: kadai5 Template

--- Kadai6/Kadai6.cpp
+++ Kadai6/Kadai6.cpp
@@ -1,20 +1,71 @@
-﻿// Kadai6.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
+﻿#include <opencv2/opencv.hpp>
 
-#include <iostream>
+using namespace cv;
+
+#ifdef _DEBUG
+// debug用のライブラリをリンク
+#pragma comment(lib, "opencv_legacy2413d.lib")
+#pragma comment(lib, "opencv_calib3d2413d.lib")
+#pragma comment(lib, "opencv_core2413d.lib")
+#pragma comment(lib, "opencv_objdetect2413d.lib")
+#pragma comment(lib, "opencv_features2d2413d.lib")
+#pragma comment(lib, "opencv_imgproc2413d.lib")
+#pragma comment(lib, "opencv_highgui2413d.lib")
+#else
+// Release用のライブラリをリンク
+#pragma comment(lib, "opencv_legacy2413.lib")
+#pragma comment(lib, "opencv_core2413.lib")
+#pragma comment(lib, "opencv_imgproc2413.lib")
+#pragma comment(lib, "opencv_highgui2413.lib")
+#pragma comment(lib, "opencv_objdetect2413.lib")
+#pragma comment(lib, "opencv_features2d2413.lib")
+#pragma comment(lib, "opencv_calib3d2413.lib")
+#endif
+
+/**
+ *  800 x ? のサイズへアスペクト比を保ったままリサイズする.
+ *
+ * @param in 入力画像
+ * @param out 出力画像
+ */
+void my_resize(const Mat& in, Mat& out)
+{
+    //! 入力画像のアスペクト比
+    const double aspect_ratio = (double)in.cols / in.rows;
+    //! 出力画像の横幅
+    constexpr int WIDTH = 800;
+    //! アスペクト比を保持した高さ
+    const int height = round(WIDTH / aspect_ratio);
+
+    // リサイズ用画像領域の確保
+    Mat resize_img(height, WIDTH, in.type());
+    // リサイズ
+    resize(in, resize_img, resize_img.size());
+    resize_img.copyTo(out);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //! 画像の読み込み
+    const Mat src_img = imread("./img/in.jpg", IMREAD_GRAYSCALE);
+    // 読み込んだ画像のNULLチェック
+    if (src_img.empty()) { return -1; }
+
+    // 読み込んだ画像のリサイズ
+    Mat resize_img;
+    my_resize(src_img, resize_img);
+
+    // BGRからYCrCbへ変換
+    Mat ycrcb_image;
+    cvtColor(resize_img, ycrcb_image, COLOR_BGR2YCrCb);
+
+    // 結果表示
+    imshow("ycrcb_image", ycrcb_image);
+
+    imwrite("./img/ycrcb_image.png", ycrcb_image);
+
+    // キーボードが押されるまでwait
+    waitKey(0);
+
+    return 0;
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
