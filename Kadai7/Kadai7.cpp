3e918dd153f72dda2ad95f4bc0a193aac46c5c00
AURAjp <basket.3522@gmail.com>
2021-10-26 21:16:06 +0900

    feat: 課題7-3に対応したロジックを追加

--- Kadai7/Kadai7.cpp
+++ Kadai7/Kadai7.cpp
@@ -22,43 +22,48 @@ using namespace cv;
 #pragma comment(lib, "opencv_calib3d2413.lib")
 #endif
 
-/**
- *  800 x ? のサイズへアスペクト比を保ったままリサイズする.
- *
- * @param in 入力画像
- * @param out 出力画像
- */
-void my_resize(const Mat& in, Mat& out)
-{
-    //! 入力画像のアスペクト比
-    const double aspect_ratio = (double)in.cols / in.rows;
-    //! 出力画像の横幅
-    constexpr int WIDTH = 800;
-    //! アスペクト比を保持した高さ
-    const int height = round(WIDTH / aspect_ratio);
-
-    // リサイズ用画像領域の確保
-    Mat resize_img(height, WIDTH, in.type());
-    // リサイズ
-    resize(in, resize_img, resize_img.size());
-    resize_img.copyTo(out);
-}
-
 int main()
 {
     //! 画像をグレースケールで読み込み
-    const Mat src_img = imread("../img/in.jpg", IMREAD_GRAYSCALE);
+    const Mat in_img = imread("../img/kadai7/in.png", IMREAD_GRAYSCALE);
+    const Mat tmp_img = imread("../img/kadai7/tmp.png", IMREAD_GRAYSCALE);
     // 読み込んだ画像のNULLチェック
-    if (src_img.empty()) { return -1; }
+    if (in_img.empty() && tmp_img.empty()) { return -1; }
 
-    // 読み込んだ画像のリサイズ
-    Mat in_img;
-    my_resize(src_img, in_img);
+    //! 類似の箇所の出力結果を保存するための領域
+    Mat SSD, CC, ZNCC;
+
+    // 類似箇所を検索
+    matchTemplate(in_img, tmp_img, SSD , CV_TM_SQDIFF       );
+    matchTemplate(in_img, tmp_img, CC  , CV_TM_CCORR        );
+    matchTemplate(in_img, tmp_img, ZNCC, CV_TM_CCOEFF_NORMED);
+
+    //! 探索結果の位置
+    Point pos_SSD;
+    Point pos_CC;
+    Point pos_ZNCC;
+
+    // 相互相関マップの最大値検索
+    minMaxLoc(SSD , NULL, NULL, NULL, &pos_SSD );
+    minMaxLoc(CC  , NULL, NULL, NULL, &pos_CC  );
+    minMaxLoc(ZNCC, NULL, NULL, NULL, &pos_ZNCC);
+
+    /**
+     * 類似箇所を色付きの四角で囲みたいため、
+     * 出力用画像はグレースケールではなくBGRで用意する
+     */
+    Mat out_img = in_img.clone();
+    cvtColor(out_img, out_img, CV_GRAY2BGR);
+
+    // 類似箇所を色付きの四角で囲む
+    rectangle(out_img, Rect(pos_SSD.x , pos_SSD.y , tmp_img.cols, tmp_img.rows), Scalar(0, 0, 255), 2);
+    rectangle(out_img, Rect(pos_CC.x  , pos_CC.y  , tmp_img.cols, tmp_img.rows), Scalar(0, 255, 0), 2);
+    rectangle(out_img, Rect(pos_ZNCC.x, pos_ZNCC.y, tmp_img.cols, tmp_img.rows), Scalar(255, 0, 0), 2);
 
     // 結果表示
-    imshow("in", in_img);
+    imshow("out", out_img);
 
-    imwrite("../img/kadai7/in.png", in_img);
+    imwrite("../img/kadai7/out.png", out_img);
 
     // キーボードが押されるまでwait
     waitKey(0);
