947daa9f82be7bd8354699f8ab9febd49801e1c5
AURAjp <basket.3522@gmail.com>
2021-10-26 11:30:40 +0900

    feat: kadai7 のテンプレートを追加

---
+++ Kadai7/Kadai7.cpp
@@ -0,0 +1,67 @@
+﻿#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+#ifdef _DEBUG
+// debug用のライブラリをリンク
+#pragma comment(lib, "opencv_legacy2413d.lib")
+#pragma comment(lib, "opencv_calib3d2413d.lib")
+#pragma comment(lib, "opencv_core2413d.lib")
+#pragma comment(lib, "opencv_objdetect2413d.lib")
+#pragma comment(lib, "opencv_features2d2413d.lib")
+#pragma comment(lib, "opencv_imgproc2413d.lib")
+#pragma comment(lib, "opencv_highgui2413d.lib")
+#else
+// Release用のライブラリをリンク
+#pragma comment(lib, "opencv_legacy2413.lib")
+#pragma comment(lib, "opencv_core2413.lib")
+#pragma comment(lib, "opencv_imgproc2413.lib")
+#pragma comment(lib, "opencv_highgui2413.lib")
+#pragma comment(lib, "opencv_objdetect2413.lib")
+#pragma comment(lib, "opencv_features2d2413.lib")
+#pragma comment(lib, "opencv_calib3d2413.lib")
+#endif
+
+/**
+ *  800 x ? のサイズへアスペクト比を保ったままリサイズする.
+ *
+ * @param in 入力画像
+ * @param out 出力画像
+ */
+void my_resize(const Mat& in, Mat& out)
+{
+    //! 入力画像のアスペクト比
+    const double aspect_ratio = (double)in.cols / in.rows;
+    //! 出力画像の横幅
+    constexpr int WIDTH = 800;
+    //! アスペクト比を保持した高さ
+    const int height = round(WIDTH / aspect_ratio);
+
+    // リサイズ用画像領域の確保
+    Mat resize_img(height, WIDTH, in.type());
+    // リサイズ
+    resize(in, resize_img, resize_img.size());
+    resize_img.copyTo(out);
+}
+
+int main()
+{
+    //! 画像の読み込み
+    const Mat src_img = imread("../img/in.jpg", IMREAD_COLOR);
+    // 読み込んだ画像のNULLチェック
+    if (src_img.empty()) { return -1; }
+
+    // 読み込んだ画像のリサイズ
+    Mat resize_img;
+    my_resize(src_img, resize_img);
+
+    // 結果表示
+    imshow("resize", resize_img);
+
+    imwrite("../img/kadai7/resize_img.png", resize_img);
+
+    // キーボードが押されるまでwait
+    waitKey(0);
+
+    return 0;
+}
