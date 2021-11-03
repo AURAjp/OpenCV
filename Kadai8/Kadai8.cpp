cd19eb48a55f67b63c082ea4186240c92057c08c
AURAjp <basket.3522@gmail.com>
2021-11-03 23:24:57 +0900

    feat: 画像を回転させながら照合を行う処理を追加

--- Kadai8/Kadai8.cpp
+++ Kadai8/Kadai8.cpp
@@ -73,19 +73,31 @@ void create_random_image(const Mat& in, Mat& out)
  * 画像をテンプレートマッチングで解析する。
  * 
  * @param in 解析対象画像
- * @param temp テンプレート
+ * @param tmp テンプレート
  * @param out 出力画像
  */
 void analyze_image(const Mat& in, const Mat& tmp, Mat& out)
 {
-    Mat max_pos;
-    matchTemplate(in, tmp, max_pos, CV_TM_CCOEFF_NORMED);
-    Point pos;
-    minMaxLoc(max_pos, NULL, NULL, NULL, &pos);
-
     out = in.clone();
     cvtColor(out, out, CV_GRAY2BGR);
-    rectangle(out, Rect(pos.x, pos.y, tmp.cols, tmp.rows), Scalar(0, 0, 255), 2);
+
+    Point2f center = Point2f(tmp.cols / 2.f, tmp.rows / 2.f);
+    constexpr double scale = 1;
+    constexpr double degree = 24;
+
+    // 回転させながら照合を行う
+    for (double i = 0; i < 360; i += degree)
+    {
+        Mat change_src = getRotationMatrix2D(center, i, scale);
+        Mat rotated_img;
+        warpAffine(tmp, rotated_img, change_src, tmp.size());
+
+        Mat max_pos;
+        matchTemplate(in, rotated_img, max_pos, CV_TM_CCOEFF_NORMED);
+        Point pos;
+        minMaxLoc(max_pos, NULL, NULL, NULL, &pos);
+        rectangle(out, Rect(pos.x, pos.y, rotated_img.cols, rotated_img.rows), Scalar(0, 0, 255), 2);
+    }
 }
 
 int main()
@@ -104,8 +116,12 @@ int main()
     analyze_image(input_img, src_img, out_img);
 
     // 結果表示
+    imshow("src_img", src_img);
     imshow("input_img", input_img);
     imshow("out_img", out_img);
+
+    imwrite("../img/kadai8/in.png", input_img);
+    imwrite("../img/kadai8/out.png", out_img);
 
     // キーボードが押されるまでwait
     waitKey(0);
